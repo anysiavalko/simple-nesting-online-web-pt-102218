33c10948a1c2bbfad1401d3cc688312912a11c75
Anysia V <anysiavalko@gmail.com>
2018-11-08 21:13:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,6 +58,7 @@ def dennis_ritchies_language
 programmer_hash[:dennis_ritchie][:languages].join     
 end
 
+
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -80,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
     
 end
 
